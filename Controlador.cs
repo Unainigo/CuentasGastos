9ac17775fbe1e5618fea078fc86fbcbd742f3264
unaii <unaii@DESKTOP-2QULQ1S>
2025-03-14 00:02:12 +0100

    Agregar archivos de proyecto.

---
+++ Controlador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+public class controlador
+{
+	public controlador()
+	{
+		string connetionString = "datasource=127.0.0.1;port=3306;user=root;password=;database=finanazas";
+		string query = "select * From Persona";
+		MySqlConnection databaseConnection = new MySqlConnection(connetionString);
+		MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+		MySqlDataReader reader;
+        try
+        {
+			databaseConnection.Open();
+			reader = databaseConnection.ExecuteReader();
+
+			if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+					
+				}
+			}
+            else
+            {
+				Console.WriteLine("No se encontro nada");
+            }
+			databaseConnection.Close();
+        }
+		catch(Exception e)
+        {
+			MessageBox.Show(e.Message);
+        }
+	}
+}
---
+++ Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class usuario{
+	private int id_Usuario { set; get; };
+	private string nombre { set; get; }
+	private string correo { set; get; }
+	private string contraseña { set; get; }
+	private List <cuentaAhorro> cuentas { set; get; }
+	public Usuario(int id_Usuario, string nombre, string correo, string contraseña){
+		this.id_Usuario = id_Usuario;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.contraseña = contraseña;
+		this.cuentas = new	List<cuentaAhorro>();
+	}
+	private void añadirCuenta(cuentaAhorro cuenta){
+		cuentas.add(cuenta);
+    }
+	private void cambiarContraseña(string nuevaContraseña){
+		contraseña = nuevaContraseña;
+    }
+}
