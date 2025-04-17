77cf3e9e3862c44bc5496c77b291f3ffc3c5838a
unai <unainigo2@gmail.com>
2025-04-17 17:29:28 +0200

    se modifica el añadir el telefono al usuario

--- Usuario.cs
+++ Usuario.cs
@@ -7,12 +7,14 @@ public class usuario{
 	private string nombre { set; get; }
 	private string correo { set; get; }
 	private string contraseña { set; get; }
+	private string Telefono { set; get; }
 	private List <cuentaAhorro> cuentas { set; get; }
-	public Usuario(int id_Usuario, string nombre, string correo, string contraseña){
+	public Usuario(int id_Usuario, string nombre, string correo, string contraseña, string telefono){
 		this.id_Usuario = id_Usuario;
 		this.nombre = nombre;
 		this.correo = correo;
 		this.contraseña = contraseña;
+		this.Telefono = telefono;
 		this.cuentas = new	List<cuentaAhorro>();
 	}
 	private void añadirCuenta(cuentaAhorro cuenta){
